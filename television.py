ccf45473deb0d36c2ec36bf3e12bbe95a1ecba58
TheEisenfaust <ssgt_e_brewer@yahoo.com>
2025-04-25 19:49:04 -0500

    Update television.py

--- television.py
+++ television.py
@@ -51,6 +51,6 @@ class Television:
 
     def __str__(self):
         if self.__muted:
-            return f'Power = {self.__status}, Channel = {self.__channel} Volume = {Television.MIN_Volume}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_Volume}'
         else:
-            return f'Power = {self.__status}, Channel = {self.__channel} Volume = {self.__volume}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
